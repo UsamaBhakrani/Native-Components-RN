a8bcd4d6fab86ea3cc0dc98b69bbafc5f50ab75b
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-07 13:16:26 +0500

    FlatList props

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,23 +1,26 @@
 import { Text, View, FlatList, StyleSheet } from "react-native";
 
+const friends = [
+  { id: 1, name: "Friend 1" },
+  { id: 2, name: "Friend 2" },
+  { id: 3, name: "Friend 3" },
+  { id: 4, name: "Friend 4" },
+  { id: 5, name: "Friend 5" },
+  { id: 6, name: "Friend 6" },
+  { id: 7, name: "Friend 7" },
+  { id: 8, name: "Friend 8" },
+  { id: 9, name: "Friend 9" },
+  { id: 10, name: "Friend 10" },
+];
 const ListScreen = () => {
-  const friends = [
-    { id: 1, name: "Friend 1" },
-    { id: 2, name: "Friend 2" },
-    { id: 3, name: "Friend 3" },
-    { id: 4, name: "Friend 4" },
-    { id: 5, name: "Friend 5" },
-    { id: 6, name: "Friend 6" },
-    { id: 7, name: "Friend 7" },
-    { id: 8, name: "Friend 8" },
-    { id: 9, name: "Friend 9" },
-    { id: 10, name: "Friend 10" },
-  ];
   return (
     <FlatList
+      showsHorizontalScrollIndicator={false}
+      horizontal
+      keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text key={item.id}>{item.name}</Text>;
+        return <Text style={{ padding: 20 }}>{item.name}</Text>;
       }}
     />
   );
