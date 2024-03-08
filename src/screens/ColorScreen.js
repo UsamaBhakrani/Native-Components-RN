bff1eecddf4cf08323e7caa14e0d08590bdef4fb
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-08 18:22:55 +0500

    Color Screen

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,12 +1,20 @@
 import { Button, StyleSheet, View } from "react-native";
 
 const ColorScreen = () => {
-
   return (
     <View>
-      <Button title="Generate Color" onPress={()=>{}} />
+      <Button title="Generate Color" onPress={() => {}} />
+      <View style={styles.container} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    width: 100,
+    backgroundColor: `rgb(0,255,0)`,
+  },
+});
 
 export default ColorScreen;
